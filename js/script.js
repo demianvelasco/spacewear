526e166be20b0786e61d705b0d8438a3170d30d3
kelcel <keith.hargett@knights.ucf.edu>
2014-04-13 01:56:39 -0400

    Python Files, Web.py Library, etc

--- js/script.js
+++ js/script.js
@@ -8,19 +8,4 @@ $(document).ready(function(){
 function blink()
 {
 	// create JSON object
-	blink = JSON.stringify({speed: val});
-
-	// create request object
-	request = new XMLHttpRequestObject();
-
-	// open server file
-	request.open("POST", "filename.py", true);
-
-	// set request header
-	request.setRequestHeader("Content-type", "application/json");
-
-	// send data
-	request.send(blink);
-
-	// code to display response?*/
 }
