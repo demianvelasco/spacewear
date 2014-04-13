f125b13bd0861e4831a50b2fe99fa82135dfea24
kelcel <keith.hargett@knights.ucf.edu>
2014-04-12 20:15:50 -0400

    Added JSON Functionality

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
 	
 	// jQuery code once document loaded
-
+	val = 500;
 });
 
 // blink led function
 function myFunction()
 {
-	alert("LED lit.");
+	// create JSON object
+	json = JSON.stringify({ x: val});
+
+	obj = JSON.parse(json);
+
+	alert(obj.x);
+	/*
+	// create request object
+	request = new XMLHttpRequestObject();
+
+	// open server file
+	////request.open("POST", "filename.py", true);
+
+	// set request header
+	request.setRequestHeader("Content-type", "application/json");
+
+	// send data
+	request.send(str_json);
+
+	// code to display response?*/
 }
